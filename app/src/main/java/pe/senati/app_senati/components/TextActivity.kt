e2bd82f03ccd220a265bcc0a83efdc85ce11c3f1
Aldo Omar Morales Carlos <aomc06@gmail.com>
2025-03-26 20:56:37 -0500

    Add componentes

---
+++ app/src/main/java/pe/senati/app_senati/components/TextActivity.kt
@@ -0,0 +1,50 @@
+package pe.senati.app_senati.components
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Toast
+
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
+import pe.senati.app_senati.R
+
+
+class TextActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_text)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        var textViewTitle = findViewById<TextView>(R.id.textViewTitle)
+        textViewTitle.setText("Bienvenidos todos!")
+        textViewTitle.setTextColor(Color.RED)
+        textViewTitle.setOnClickListener {
+            Toast.makeText(this, "click en el titulo", Toast.LENGTH_SHORT).show()
+        }
+
+        var editTextTextSurname = findViewById<TextView>(R.id.editTextTextSurname)
+        editTextTextSurname.addTextChangedListener {
+            if(editTextTextSurname.length() == 0) {
+                editTextTextSurname.setError("Campo requerido")
+            }
+        }
+
+        var autoCompleteTextView = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
+        var countries = resources.getStringArray(R.array.countries_array)
+        var adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, countries)
+        autoCompleteTextView.setAdapter(adapter)
+
+    }
+}
